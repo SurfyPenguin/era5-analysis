9d1a7eb35533bea86e3826e80ae9a01185376fe3
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-03-22 17:18:09 +0530

    feat: update downloader script

--- src/scripts/download.py
+++ src/scripts/download.py
@@ -1,57 +1,63 @@
+from collections.abc import Iterator
 import os
-from pathlib import Path
+from typing import overload
 
-import yaml
+from constants import DATA_CONFIG
 
 from cds_weather_api import RequestBuilder
+from cds_weather_api.validators import Validators as validate
 
-# for terminal
-FILL_CHAR: str = "="
+def month_range(chunk: int) -> Iterator[tuple[int, int]]:
+    for start_month in range(1, 13, chunk):
+        end_month: int = min(start_month + chunk - 1, 12)
+        yield start_month, end_month
 
-# assumes script is at src/scripts/download.py
-CONFIG_DIR: Path = Path(__file__).parent.parent.parent / "config"
+def download_year(year: int, output_dir: str | None = None) -> None:
+    era5_config: dict = DATA_CONFIG["era5"]
+    fmt: str = era5_config["download_format"]
 
-def load_config(name: str) -> dict:
-    with open(CONFIG_DIR / f"{name}.yaml") as file:
-        return yaml.safe_load(file)
+    def get_target(start: int, end: int) -> str:
+        file_name: str = f"{year}_{start}-{end}.{fmt}"
 
-def download() -> None:
-    config: dict = load_config("data")
-    # working variables
-    variables: list[str] = config["era5"]["variables"]
+        if output_dir:
+            return os.path.join(output_dir, file_name)
+        return file_name
 
-    # step value for 'range()'
-    CHUNK: int = 4
+    for start, end in month_range(chunk=5):
+        target = get_target(start, end)
+        request = (
+            RequestBuilder()
+            .dataset(era5_config["dataset"])
+            .product_type(era5_config["product_type"])
+            .variables(era5_config["variables"])
+            .year(year)
+            .month_range(start, end)
+            .day_range(1, 31)
+            .time_range(0, 23)
+            .area(era5_config["bounding_box"])
+            .download_format(era5_config["download_format"])
+            .data_format(era5_config["data_format"])
+            .target(file_name=target)
+            .build()
+        )
+        print(f">> Started download: {target}")
+        request.execute()
 
-    for year in range(2020, 2025):
-        for month in range(1, 13, CHUNK):
+@overload
+def download(start_year: int, *, output_dir: str | None = None) -> None: ...
 
-            end_month = min(month + CHUNK -1, 13)
-            file_name = f"{year}_{month}-{end_month}.zip"
+@overload
+def download(start_year: int, end_year: int, *, output_dir: str | None = None) -> None: ...
 
-            request = (
-                RequestBuilder()
-                .dataset("reanalysis-era5-single-levels")
-                .product_type("reanalysis")
-                .variables(variables)
-                .year(year)
-                .month_range(month, end_month)
-                .day_range(1, 31)
-                .time_range(0, 23)
-                .data_format("netcdf")
-                .area((35, 65, 5, 95))
-                .download_format("zip")
-                .target(file_name=file_name)
-                .build()
-            )
-            print(f">> Started download: {file_name}")
-            request.execute()
+def download(*args, **kwargs):
+    # validate
+    validate.years(args)
 
-def main() -> None:
-    terminal_width: int = os.get_terminal_size()[0]
-    print("Dataset Download Script".center(terminal_width, FILL_CHAR))
+    if len(args) == 1:
+        download_year(*args, **kwargs)
 
-    download()
-
-if __name__ == "__main__":
-    main()
+    if len(args) == 2:
+        start: int = args[0]
+        end: int = args[-1]
+        for year in range(start, end + 1):
+            download_year(year, **kwargs)
